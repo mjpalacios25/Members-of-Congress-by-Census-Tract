61c5c528c5db1dbd4690e938e65a579bddd10067
Zapata <mospalacios@gmail.com>
2023-02-16 16:22:40 -0500

    First commit for main script

---
+++ Congressional District Script w NCES Geo.R
@@ -0,0 +1,48 @@
+library("dplyr")
+library("purrr")
+library("readxl")
+library("rebus")
+library("stringr")
+library("tidyr")
+library("readr")
+
+rm(list = ls())
+
+#Read in files, and bind rows on Census File
+CensusFile <- list.files() %>% str_subset(pattern = START %R% "grf")
+CensusCongressionalFile <- read_xlsx(CensusFile) %>% separate(col = "STCD116", into = c("STATE", "CD"), 2)
+StateCodes <- read_xlsx("State Codes.xlsx")
+
+#need recode of state abbrv. to state numbers
+HouseMembers <- read_excel("House XML.xlsx") %>%
+  select("statedistrict", "last_name" = "member-info.lastname", "first_name" = "member-info.firstname",
+        "official_name" =  "member-info.official-name", "party" = "member-info.party", "state" =  "member-info.state",
+        "district" = "member-info.district", "town+name" =  "member-info.townname") %>%
+  separate(col = "statedistrict", into = c("State", "CongressionalDistrict"), sep = 2) %>%
+  left_join(StateCodes, by = c("State" = "STUSAB")) %>%
+  unite("HouseMemberwParty", "first_name", "last_name", "party", sep = ", ")
+
+SenateMembers <- read_excel("Senate XML.xlsx") %>%
+  select(1:5) %>%
+  left_join(StateCodes, by = c("state" = "STUSAB")) %>%
+  unite("SenateMemberwParty", "first_name", "last_name", "party", sep = ", ") %>%
+  group_by(state) %>%
+  mutate(Senator = seq_along(1:2)) %>%
+  pivot_wider(id_cols = state, names_from = Senator, values_from = SenateMemberwParty, names_prefix = "Senator ")
+
+CGCSDistricts <- read_csv("CGCS NCES IDs for Congress Member Merge.csv") %>%
+  select("LEAID", "LEA_NAME")
+CGCSDistricts$LEAID <- sprintf("%07d", CGCSDistricts$LEAID)
+
+#Filter Census File by CGCS District LEAIDs
+vars <- c("LEAID", "NAME_LEA21", "State", "STATE", "CD", "COUNT", "district", "town+name", "HouseMemberwParty", "Senator 1", "Senator 2")
+
+CensusFiltered <- CensusCongressionalFile %>%
+  inner_join(CGCSDistricts, by =  c("LEAID")) %>%
+  left_join(HouseMembers, by = c("STATE", "CD" = "CongressionalDistrict")) %>%
+  left_join(SenateMembers, by = c("State" = "state")) %>%
+  select(!!vars)
+  
+
+#Save File
+write_csv(CensusFiltered, "CGCS Members of Congress.csv")
